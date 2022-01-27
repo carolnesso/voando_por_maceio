989aa474587208f774c843d1ce6d90e84a56dd68
Carolnesso <acng@ic.ufal.br>
2022-01-27 00:26:08 -0300

    ajusta o freeglut

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto de computação grafica!
+### Os prerequisitos do projeto são: 
+    Modelar um avião e utilizar textura e iluminação nele.
+    Simular um sobrevoo pela cidade de Maceió
+    Controlar o avião utilizando o teclado.
+    Modelar a cidade de Maceió utilizando mapeamento de textura. 
+
+##### Aluna: Ana Carolina Nesso Guedes
--- airplane.c
+++ airplane.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
+void start()
+{
+    glClearColor(0, 0, 0, 0);
+}
+void draw(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.5, -0.5, -0.5);
+    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(0.5, 0.5, 0.5);
+
+
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -9,11 +25,9 @@ int main(int argc, char** argv)
     glutInitWindowPosition(200, 200);
     glutCreateWindow("Voando por Maceió");
 
-    
-    glutDialsFunc(display);
-    glutKeyboardFunc(keyboard);
+    // start();
+    // glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
 
-    
 }
