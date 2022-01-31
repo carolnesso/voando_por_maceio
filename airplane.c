639a049833f4bc20f61123016a5dd6106cf5d80e
Ana Carolina <carolnesso@gmail.com>
2022-01-31 02:38:35 -0300

    ajusta o leitor de .Obj

--- airplane.c
+++ airplane.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 #include <GL/glut.h>
+#include <stdio.h>
+
+typedef struct Vertex
+{
+    float x, y, z; //posição do vertice
+    // float r, g, b; //cor do vertice
+}Vertex;
+
 
 void start()
 {
     glClearColor(0, 0, 0, 0);
 }
-void draw(void)
+
+    Vertex vertices[] = {
+        {0.8f, 0.8f, 0.8f},
+        { 0.8f,  0.8f, 0.8f},
+        {0.8f, 0.8f, 0.8f},
+    };
+
+void display()
 {
+    int i;
+    Vertex v;
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    
+    for(i = 0; i < sizeof(vertices); i++)
+    {
+        v = vertices[i];
+        glVertex3f(v.x, v.y, v.z);
+    }
 
-    glBegin(GL_POLYGON);
-    glVertex3f(-0.5, -0.5, -0.5);
-    glVertex3f(0.5, -0.5, 0.5);
-    glVertex3f(0.5, 0.5, 0.5);
-
+    glEnd();
+    glutSwapBuffers();
 
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 400);
     glutInitWindowPosition(200, 200);
-    glutCreateWindow("Voando por Maceió");
+    glutCreateWindow("Voando por Maceio");
 
-    // start();
-    // glutDisplayFunc(draw);
+    start();
+    glutDisplayFunc(display);
     glutMainLoop();
+
     return 0;
 
 }
